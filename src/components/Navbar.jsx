604c033740abd8879c0b0c6e784044d6453a6358
alexblack24 <alex.black@mail.com>
2024-09-04 15:02:08 +0200

    Navbar Imports Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from "react"
+import {Link} from "react-router-dom"
+import {styles} from "../styles"
+import {navLinks} from "../constants"
+import {logo, menu, close} from "../assets"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  return <div></div>
 }
 
 export default Navbar
