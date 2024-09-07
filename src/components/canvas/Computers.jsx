3421b5206cfd7f1d0af3005aea6c72f0da00bd05
alexblack24 <alex.black@mail.com>
2024-09-07 16:49:37 +0200

    loading added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,8 +10,8 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0} groundColor="black" />
       <pointLight intensity={0.8} />
-      <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} intensity={1100} castShadow shadow-mapSize={1024} />
-      <primitive object={computer.scene} scale={0.75} position={[0, -4.5, -1.5]} rotation={[0, -0.2, -0.05]} />
+      <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} intensity={900} castShadow shadow-mapSize={1024} />
+      <primitive object={computer.scene} scale={0.75} position={[0, -4, -1.5]} rotation={[0, -0.2, -0.05]} />
     </mesh>
   )
 }
@@ -19,7 +19,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas frameloop="demand" shadows camera={{position: [20, 3, 5], fov: 25}} gl={{preserveDrawingBuffer: true}}>
-      <Suspense>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Computers />
       </Suspense>
