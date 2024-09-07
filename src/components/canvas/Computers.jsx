162a27cfebc8fc68a4b271fbf807c9415e010c60
alexblack24 <alex.black@mail.com>
2024-09-07 12:03:40 +0200

    3D Model working

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,20 +5,19 @@ import {OrbitControls, Preload, useGLTF} from "@react-three/drei"
 import CanvasLoader from "../Loader"
 
 const Computers = () => {
-  const computer = useGLTF("./macbook/scene.gltf")
+  const computer = useGLTF("./desktop_pc/scene.gltf")
   return (
     <mesh>
-      <hemisphereLight intensity={1} groundColor="black" />
-      {/* <pointLight intensity={0.8} /> */}
-      {/* <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} intensity={1100} castShadow shadow-mapSize={1024} /> */}
-      <primitive object={computer.scene} scale={2.1} position={[0, 0, 0]} />
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} scale={0.75} position={[0, -5, -1.5]} />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
   return (
-    <Canvas frameloop="demand" shadows camera={{position: [20, 0, 0], fov: 100}} gl={{preserveDrawingBuffer: true}}>
+    <Canvas frameloop="demand" shadows camera={{position: [20, 3, 5], fov: 25}} gl={{preserveDrawingBuffer: true}}>
       <Suspense>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Computers />
