ab2bb2200f2404f26e28f8759b452deae1346762
alexblack24 <alex.black@mail.com>
2024-09-07 12:35:00 +0200

    3D Model Working well

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import {Suspense, useEffect, useState} from "react"
 import {Canvas} from "@react-three/fiber"
-import {OrbitControls, Preload, useGLTF} from "@react-three/drei"
+import {OrbitControls, Preload, SpotLight, useGLTF} from "@react-three/drei"
 
 import CanvasLoader from "../Loader"
 
@@ -8,9 +8,10 @@ const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf")
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} scale={0.75} position={[0, -5, -1.5]} />
+      <hemisphereLight intensity={0} groundColor="black" />
+      <pointLight intensity={0.8} />
+      <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} intensity={1100} castShadow shadow-mapSize={1024} />
+      <primitive object={computer.scene} scale={0.75} position={[0, -4.5, -1.5]} rotation={[0, -0.2, -0.05]} />
     </mesh>
   )
 }
