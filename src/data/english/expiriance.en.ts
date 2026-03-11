f649c8e753800b4b1c91fce19253e7b2bff13c61
alexbalak21 <alex.balak@outlook.com>
2026-03-11 13:06:19 +0100

    created work history / experience

---
+++ src/data/english/expiriance.en.ts
@@ -0,0 +1,48 @@
+import novocib from "/images/history/novocib.png";
+import kardol from "/images/history/kardol.png";
+import taqt from "/images/history/taqt.png";
+import cgi from "/images/history/cgi.png";
+import flyit from "/images/history/flyit.png";
+
+export const history = [
+  {
+    role: "Full Stack Developer",
+    organisation: "Novocib",
+    startDate: "Jun, 2025",
+    endDate: "Jan, 2026",
+    experiences: ["Designed the new modern website", "Improved SCO & visibility, increased sails by 30%", "Implemented CMS for product management"],
+    imageSrc: novocib,
+  },
+  {
+    role: "Sage X3 Developer",
+    organisation: "Kardol",
+    startDate: "Oct, 2024",
+    endDate: "Apr, 2025",
+    experiences: ["Addapted the software for clients needs", "Maintained the ERP system", "Provided user support"],
+    imageSrc: kardol,
+  },
+  {
+    role: "Support Engineer",
+    organisation: "Taqt",
+    startDate: "Nov, 2023",
+    endDate: "Jan, 2024",
+    experiences: ["Worked & maintened IOT devices", "Worked with REST API", "Provided hardware & software user support"],
+    imageSrc: taqt,
+  },
+  {
+    role: "Support Engineer",
+    organisation: "CGI",
+    startDate: "Jul, 2022",
+    endDate: "Oct, 2023",
+    experiences: ["Worked on 3D design program", "Helped users with technical issues", "Build program to mesure performance"],
+    imageSrc: cgi,
+  },
+  {
+    role: "Full Stack Developer",
+    organisation: "Fly It",
+    startDate: "Oct, 2023",
+    endDate: "Nov, 2023",
+    experiences: ["Coded the company website", "Improved load time & maintainability"],
+    imageSrc: flyit,
+  },
+]
